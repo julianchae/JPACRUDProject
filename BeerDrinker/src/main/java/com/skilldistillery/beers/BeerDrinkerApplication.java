b53f7982a861674b60aa6d389bb010ddbdbaedb1
Julian Chae <Julianchae9@gmail.com>
2022-04-15 19:30:43 -0600

    Start Boot mvc

---
+++ BeerDrinker/src/main/java/com/skilldistillery/beers/BeerDrinkerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerDrinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerDrinkerApplication.class, args);
+	}
+
+}
---
+++ BeerDrinker/src/test/java/com/skilldistillery/beers/BeerDrinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerDrinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
